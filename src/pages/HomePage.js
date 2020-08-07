00563f43d2af2ed77307835e01ad46b8243b0a9e
Patrick Brown <patrickbrown1312@gmail.com>
2020-08-07 16:49:05 -0700

    Created homepage, did basic styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { authEndpoint, clientId, redirectUri, scopes } from "../config";
 import hash from "../hash";
 import axios from 'axios';
+import "./HomePage.css";
 export default class HomePage extends Component {
   constructor() {
     super();
@@ -19,7 +20,9 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="HomePage">
-        <h1>Welcome to the home page!</h1>
+        <div className="background"></div>
+        <h1 className="title-header">Playlist Analyzer</h1>
+        <h2 className="subtitle-header">Discover your music</h2>
       </div>
     );
   }
