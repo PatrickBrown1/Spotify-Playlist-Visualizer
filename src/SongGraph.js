132a55eaa0b9493e11f4d5ea25fc91442b740a05
Patrick Brown <patrickbrown1312@gmail.com>
2020-08-17 13:01:01 -0700

    Domain/range now change based on what song traits they represent

--- src/SongGraph.js
+++ src/SongGraph.js
@@ -1,40 +1,112 @@
 import React, { Component } from "react";
 
-import { VictoryTheme, VictoryScatter, VictoryChart, VictoryLegend } from "victory";
+import { VictoryTheme, VictoryScatter, VictoryChart, VictoryAxis } from "victory";
 export default class SongGraph extends Component {
   constructor(props) {
     super();
     this.state = {
     };
     console.log(props.allSongsArray);
+    this.createChartData = this.createChartData.bind(this);
+    this.calculateDomain = this.calculateDomain.bind(this);
   }
 
   componentDidMount() {
-   
   }
 
   componentWillUnmount() {
 
+  }
+  createChartData(domainName, rangeName){
+    var dataset = [];
+    //need to create a data set with x and y
+    this.props.allSongsArray.forEach(songObj => {
+      dataset.push({
+        x: songObj["song_analysis"][domainName],
+        y: songObj["song_analysis"][rangeName]
+      })
+    });
+    return dataset;
+  }
+  calculateDomain(domainName, rangeName){
+    var domainObj = {};
+    switch(domainName){
+      case "acousticness":
+        domainObj.x = [0, 1];
+        break;
+      case "danceability":
+        domainObj.x = [0, 1];
+        break;
+      case "energy":
+        domainObj.x = [0, 1];
+        break;
+      case "instrumentalness":
+        domainObj.x = [0, 1];
+        break;
+      case "liveness":
+        domainObj.x = [0, 1];
+        break;
+      case "loudness":
+        domainObj.x = [-60, 0];
+        break;
+      case "speechiness":
+        domainObj.x = [0, 1];
+        break;
+      case "valence":
+        domainObj.x = [0, 1];
+        break;
+      case "tempo":
+        domainObj.x = [0, 200];
+        break;
+      default:
+        domainObj.x = [0, 100];
+    }
+    switch(rangeName){
+      case "acousticness":
+        domainObj.y = [0, 1];
+        break;
+      case "danceability":
+        domainObj.y = [0, 1];
+        break;
+      case "energy":
+        domainObj.y = [0, 1];
+        break;
+      case "instrumentalness":
+        domainObj.y = [0, 1];
+        break;
+      case "liveness":
+        domainObj.y = [0, 1];
+        break;
+      case "loudness":
+        domainObj.y = [-60, 0];
+        break;
+      case "speechiness":
+        domainObj.y = [0, 1];
+        break;
+      case "valence":
+        domainObj.y = [0, 1];
+        break;
+      case "tempo":
+        domainObj.y = [0, 200];
+        break;
+      default:
+        domainObj.y = [0, 100];
+    }
+    return domainObj;
   }
   render() {
     return (
       <div>
         <VictoryChart
             theme={VictoryTheme.material}
-            domain={{ x: [0, 5], y: [0, 7] }}
+            domain={this.calculateDomain("danceability", "energy")}
             width={600}
             height={400}
         >
             <VictoryScatter
                 style={{ data: { fill: "#c43a31" } }}
                 size={7}
-                data={[
-                { x: 1, y: 2 },
-                { x: 2, y: 3 },
-                { x: 3, y: 5 },
-                { x: 4, y: 4 },
-                { x: 5, y: 7 }
-                ]}
+                data={this.createChartData("danceability", "energy")}
             />
         </VictoryChart>
       </div>
