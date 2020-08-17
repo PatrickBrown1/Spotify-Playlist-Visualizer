7852b0e6d051f5a773190b1b5ad5ee7ed88c3288
Patrick Brown <patrickbrown1312@gmail.com>
2020-08-17 15:15:53 -0700

    Data points now hoverable. Show datapoint information on hover

--- src/SongGraph.js
+++ src/SongGraph.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
-import { VictoryTheme, VictoryScatter, VictoryChart, VictoryAxis } from "victory";
+import { VictoryTheme, VictoryScatter, VictoryChart, VictoryLabel } from "victory";
+import "./SongGraph.css";
 export default class SongGraph extends Component {
   constructor(props) {
     super();
@@ -23,7 +24,8 @@ export default class SongGraph extends Component {
     this.props.allSongsArray.forEach(songObj => {
       dataset.push({
         x: songObj["song_analysis"][domainName],
-        y: songObj["song_analysis"][rangeName]
+        y: songObj["song_analysis"][rangeName],
+        label_text: songObj["name"] + ": " + songObj["song_analysis"][domainName] + "," + songObj["song_analysis"][rangeName],
       })
     });
     return dataset;
@@ -107,6 +109,47 @@ export default class SongGraph extends Component {
                 style={{ data: { fill: "#c43a31" } }}
                 size={7}
                 data={this.createChartData("danceability", "energy")}
+                labels={({datum}) => ""}
+                labelComponent={<VictoryLabel className="label-text"/>}
+                events={[
+                  {
+                    target: "data",
+                    eventHandlers: {
+                      onMouseEnter: () => {
+                        return [{
+                          target: "labels",
+                          mutation: (props) => {
+                            this.setState({A: "a"});
+                            console.log(props);
+                            return {text: props.datum.label_text};
+                          }
+                        }, {
+                          target: "data",
+                          mutation: (props) => {
+                            this.setState({A: "a"});
+                            return { style: {fill: "#dc625a" }};
+                          }
+                        }];
+                      },
+                      onMouseLeave: () => {
+                        return [{
+                          target: "labels",
+                          mutation: (props) => {
+                            this.setState({A: "a"});
+                            return {text: ""};
+                          }
+                        },
+                        {
+                          target: "data",
+                          mutation: (props) => {
+                            this.setState({A: "a"});
+                            return null;
+                          }
+                        }];
+                      }
+                    }
+                  }
+                ]}
             />
         </VictoryChart>
       </div>
