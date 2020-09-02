28c9eb73eb1da7f1e5a682e730c5121d2078d566
Patrick Brown <patrickbrown1312@gmail.com>
2020-09-02 14:12:19 -0700

    depreciated navbar component

--- src/PlaylistCard.js
+++ src/PlaylistCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./PlaylistCard.css";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
