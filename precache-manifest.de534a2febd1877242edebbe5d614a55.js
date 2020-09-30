74e1118b6c4337ff49234ecca86422e6d502aa33
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-09-30 16:01:19 -0700

    Updates

---
+++ precache-manifest.de534a2febd1877242edebbe5d614a55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135c32cc894dc18f42c0e3baca429dee",
+    "url": "/Spotify-Playlist-Visualizer/index.html"
+  },
+  {
+    "revision": "348c0baef23755b8e12f",
+    "url": "/Spotify-Playlist-Visualizer/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "671d304f8f6e3f1ceeb8",
+    "url": "/Spotify-Playlist-Visualizer/static/css/main.ce07acf6.chunk.css"
+  },
+  {
+    "revision": "348c0baef23755b8e12f",
+    "url": "/Spotify-Playlist-Visualizer/static/js/2.33878efd.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/Spotify-Playlist-Visualizer/static/js/2.33878efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671d304f8f6e3f1ceeb8",
+    "url": "/Spotify-Playlist-Visualizer/static/js/main.e1906b8c.chunk.js"
+  },
+  {
+    "revision": "c2e6e2e163e44095746a",
+    "url": "/Spotify-Playlist-Visualizer/static/js/runtime-main.2f2c67a3.js"
+  },
+  {
+    "revision": "d364111ceecc58d6f48db1a8c092ca44",
+    "url": "/Spotify-Playlist-Visualizer/static/media/vinyl-record-svgrepo-com.d364111c.svg"
+  }
+]);
